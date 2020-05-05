96ff9bac5da9a923602a2472c2a4299ab36d4ebd
chenming <cmzhang1318@hotmail.com>
2020-05-04 23:05:34 -0400

    a relatively clean version of pre process for evaporation

--- examples/pytough_examples/1D_evaporation_TR_EOS4/python/pre_process.py
+++ examples/pytough_examples/1D_evaporation_TR_EOS4/python/pre_process.py
@@ -11,7 +11,7 @@ water_molecular_weight = 0.018
 R_value                = 8.3145
 mPmm                   = 1.e-3
 dayPs                  = 1./(3600*24)
-T_kelven               = 273.15
+T_init_c               = 25.0
 p_atm_pa               = 101.3e3
 
 dat       = t2data()
@@ -37,7 +37,7 @@ dat.parameter.update(
      'texp'           : 2.41e-05,
      'timestep'       : [1.0],
      'be'             : 2.334,
-     'default_incons' : [101.3e3, 10.99, 25.0, None],
+     'default_incons' : [p_atm_pa, 10.99, T_init_c, None],
      'relative_error' : 1.e-5})
 	 
 dat.parameter['print_interval'] = dat.parameter['max_timesteps']/20
@@ -45,7 +45,10 @@ dat.parameter['max_timestep']   = dat.parameter['tstop']/dat.parameter['max_time
 
 dat.start = True
 dat.diffusion=[[2.13e-5,     0.e-8],   [2.13e-5,     0.e-8]]
-dat.multi={'num_components': 2, 'num_equations': 3, 'num_phases': 2, 'num_secondary_parameters': 8}
+dat.multi={ 'num_components'           : 2,   # warning, the key needs to be exactly the same, no extra spacings
+            'num_equations'            : 3,
+            'num_phases'               : 2,
+            'num_secondary_parameters' : 8}
 
 # #Set MOPs:
 dat.parameter['option'][1]  = 1
@@ -81,7 +84,7 @@ r2.capillarity           = {'type': 1, 'parameters': [0., 0., 1.0]}
 dat.grid.add_rocktype(r2)
 
 
-bvol = 1.e50
+bvol    = 1.e50
 conarea = dx[0] * dy[0]
 condist = 1.e-10
   
@@ -93,14 +96,21 @@ for blk in dat.grid.blocklist:
 # #add boundary condition block at each end:
 b1 = t2block('bdy01', bvol, r2,
              ahtx      = conarea)# area for heat exchange 
-dat.grid.add_block(b1)
+#dat.grid.add_block(b1)
 
-con1 = t2connection([dat.grid.blocklist[0],b1],
+dat.grid.blocklist.insert(0,b1)
+#del dat.grid.blocklist[-1]
+
+
+con1 = t2connection([dat.grid.block['  a 1'],b1],
                     distance  = [0.5*dz[0],condist],
                     area      = conarea, 
                     direction = 3,   # which to read permeability
                     dircos    = -1)  # ISOT not BETX
-dat.grid.add_connection(con1)
+#dat.grid.add_connection(con1)
+
+dat.grid.connectionlist.insert(0,con1)
+
 
 # b2 = t2block('bdy02', bvol, dat.grid.rocktype['BOUND'])
 # dat.grid.add_block(b2)
@@ -109,28 +119,37 @@ dat.grid.add_connection(con1)
 # dat.grid.add_connection(con2)
 
 
-# #Set initial condition:
-for i in range(len(dat.grid.blocklist)-1):
-    dat.incon[str(dat.grid.blocklist[i])] = \
-            [None, [p_atm_pa+dat.grid.blocklist[i].centre[2]*(-1)*liquid_density_kgPm3*dat.parameter['gravity'], 10.01, 25.0]]
-dat.incon['bdy01'] = [None, [p_atm_pa, 0.99, 25.0]]   # what does 0.99 mean?
-dat.incon[str(dat.grid.blocklist[1])][1][1] = 10.99   # why is this needed?
+## #Set initial condition:
+#for i in range(len(dat.grid.blocklist)-1):
+#    dat.incon[str(dat.grid.blocklist[i])] = \
+#            [None, [p_atm_pa+dat.grid.blocklist[i].centre[2]*(-1)*liquid_density_kgPm3*dat.parameter['gravity'], 10.01, 25.0]]
+
+for num,key in enumerate(dat.grid.blocklist):
+    if str(key)[:3]=='  a':
+        dat.incon[str(key)] = \
+                [None, [p_atm_pa - dat.grid.block[str(key)].centre[2]*liquid_density_kgPm3*dat.parameter['gravity'], 10.01, T_init_c]]
+                #[None, [p_atm_pa+dat.grid.block[str(key)].centre[2]*(-1)*liquid_density_kgPm3*dat.parameter['gravity'], 10.01, T_init_c]]
+
+dat.incon['bdy01'] = [None, [p_atm_pa, 0.99, T_init_c]]   # what does 0.99 mean?
+dat.incon['  a 2'][1][1] = 10.99                          # why is this needed?
+
+
 
 # #add generator:
 flow_rate_mmPday = -5.
 flow_rate_kgPs   = flow_rate_mmPday*conarea*liquid_density_kgPm3*mPmm*dayPs
 gen              = t2generator(name  = 'INF 1', 
-                               block = dat.grid.blocklist[0].name,
+                               block = '  a 1',      #dat.grid.blocklist[0].name,
                                gx   = flow_rate_kgPs,
                                type = 'COM1')
 dat.add_generator(gen)
 
 
-# #--- write TOUGH2 input file ------------------------------------	
-dat.grid.blocklist.insert(0,dat.grid.blocklist[-1])
-del dat.grid.blocklist[-1]
-dat.grid.connectionlist.insert(0,dat.grid.connectionlist[-1])
-del dat.grid.connectionlist[-1]
+# #--- write TOUGH2 input file (obsolete)------------------------------------	
+#dat.grid.blocklist.insert(0,dat.grid.blocklist[-1])
+#del dat.grid.blocklist[-1]
+#dat.grid.connectionlist.insert(0,dat.grid.connectionlist[-1])
+#del dat.grid.connectionlist[-1]
 
 
 # only run tough2 no toughreact
